07c60958b4ac5a420045ba2da7a83b62b9c13101
Plutou2647 <1169446204@qq.com>
2021-11-25 21:08:16 +0800

    add plug

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,12 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+-- local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
-
+--[[
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
-
+--]]
 
 
 
