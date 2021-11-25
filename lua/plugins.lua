1e19aa7791fda2ff2e538efc342af751b2820c55
PlutouU2647 <1169446204@qq.com>
2021-11-25 17:12:07 +0800

    add theme to nightfox

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,12 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+-- local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
-
+--[[
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
-
+--]]
 
 
 
@@ -19,6 +19,7 @@ return require('packer').startup(function()
   use 'hrsh7th/nvim-compe'
   use {'nvim-lualine/lualine.nvim',requires = {'kyazdani42/nvim-web-devicons', opt = true}}
   -- use 'f3fora/cmp-spell'
+  use 'EdenEast/nightfox.nvim'
 end)
 
 
