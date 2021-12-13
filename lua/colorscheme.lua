7e9c473a43daec1c9ceb1491e8d691f1c0328ae4
Plutou2647 <1169446204@qq.com>
2021-12-13 21:43:00 +0800

    new branch pure

--- lua/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-
-vim.cmd('colorscheme duskfox')
---vim.cmd('colorscheme tokyonight')
-
-
-
--- vim.api.nvim_exec[[colorscheme duskfox]]
--- lua/nv-bufferline/init.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.termguicolors = true
-require("bufferline").setup{}
-
---vim.api.nivm_set_keymap('n', '<TAB>', ':BufferLineNext<CR>', {noremap = true, silent = true})
---vim.api.nivm_set_keymap('n', '<S-TAB>', ':BufferLinePrev<CR>', {noremap = true, silent = true})
-
-
