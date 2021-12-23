1a2bea344bfa48f4ca07e573dcae3cf1396830a5
Plutou2647 <1169446204@qq.com>
2021-12-23 17:16:11 +0800

    sitter

--- lua/plugins/nv-treesitter.lua
+++ lua/plugins/nv-treesitter.lua
@@ -1,11 +1,28 @@
-require'nvim-treesitter.configs'.setup {
+local status_ok, config = pcall(require, 'nvim-treesitter.configs')
+if not status_ok then
+    return
+end
+
+config.setup {
     ensure_installed = {"lua",'python'}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    sync_install = false,
     highlight = {
         enable = true,              -- false will disable the whole extension
-        additional_vim_regex_highlighting = false,
+        disable = {},               -- list of language that will be disabled
+        additional_vim_regex_highlighting = true,  -- false will disable additional highlighting based on vim regexp
     },
+    indent = {
+        enable = false,
+    }
 }
 
---
+--require'nvim-treesitter.configs'.setup {
+    --ensure_installed = {"lua",'python'}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    --highlight = {
+        --enable = true,              -- false will disable the whole extension
+        --additional_vim_regex_highlighting = false,
+    --},
+--}
+
 --'https://github.com/nvim-treesitter/nvim-treesitter'
 -- :TSbuffer
