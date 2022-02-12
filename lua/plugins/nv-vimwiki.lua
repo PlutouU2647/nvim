fa6f4874bfb51dfda1beabd46e5c88c2005dabd3
sigma2647 <1169446204@qq.com>
2022-02-13 06:52:22 +0800

    fit [[]]

--- lua/plugins/nv-vimwiki.lua
+++ lua/plugins/nv-vimwiki.lua
@@ -1,7 +1,9 @@
 vim.cmd([[
 let g:vimwiki_list = [{'path': '/Users/$USER/Library/Mobile Documents/iCloud~md~obsidian/Documents/MarkDown/000vimwiki',
-                      \ 'syntax': 'markdown', 'ext': '.md'}]
+                      \ 'ext': '.md'}]
 au filetype vimwiki silent! iunmap <buffer> <Tab>
 au filetype vimwiki silent! iunmap <buffer> <CR>
 au filetype vimwiki silent! iunmap <buffer> <S-Tab>
 ]])
+--let g:vimwiki_list = [{'path': '/Users/$USER/Library/Mobile Documents/iCloud~md~obsidian/Documents/MarkDown/000vimwiki',
+                      --\ 'syntax': 'markdown', 'ext': '.md'}]
