b8bc1fcb7582b0016f166caf5f57fc73d867d90c
sigma2647 <1169446204@qq.com>
2022-02-15 00:57:58 +0800

    fix cant run python wich coderunner

--- lua/plugins/nv-code-runner.lua
+++ lua/plugins/nv-code-runner.lua
@@ -1,12 +1,28 @@
--- this is a config example
+ --this is a config example
 require('code_runner').setup {
     term = {
         position = "belowright",
-        size = 8
+        size = 13
     },
-    --filetype_path = "/home/myuser/.config/nvim/code_runner.json",
-    --project_path = "/home/myuser/.config/nvim/projects.json"
+    --filetype_path = "$HOME/.config/nvim/code_runner.json",
+    --project_path = "$HOME/.config/nvim/projects.json"
+    filetype = {
+        python = "python -u",
+    },
 }
 vim.g.fileCommands = {
     python = 'python'
 }
+--
+--require('code_runner').setup {
+  --term = {
+    --size = 15,
+    --position = "belowright",
+  --},
+  --filetype = {
+    --python = "python -u",
+  --},
+--}
+--vim.g.fileCommands = {
+    --python = 'python'
+--}
