ffc1cd4296ba7b204052dc2be6cc93286f1ffbe0
sigma2647_macmini <1169446204@qq.com>
2022-06-20 02:00:29 +0800

    add

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -27,7 +27,16 @@ if fn.empty(fn.glob(install_path)) > 0 then
         'https://github.com/wbthomason/packer.nvim',
         install_path
     }
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
 end
+
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
 
 
 vim.cmd [[
@@ -47,7 +56,7 @@ use {
 
 
 },
---]]
+]]
 
 
 -- plugins
